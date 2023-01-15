d66aa199709a60f71293e1a61213ad55e2f43364
wassimxd <karimmohsin10@gmail.com>
2023-01-15 18:51:41 +0100

    Convert a Number to a String!

--- keep-hydrated.js
+++ keep-hydrated.js
@@ -2,7 +2,8 @@
 /* Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
 
 You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value. */
-// built in func Math.floor
+// built in func Math.git status
+floor;
 function litres(time) {
   return Math.floor(time * 0.5);
 }
---
+++ num-to-string.js
@@ -0,0 +1,9 @@
+//Convert a Number to a String!
+/* We need a function that can transform a number (integer) into a string.
+What ways of achieving this do you know? */
+//built in method toString()
+function numberToString(num) {
+  // Return a string of the number here!
+  return num.toString();
+}
+console.log(numberToString(-100));
