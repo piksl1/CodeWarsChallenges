1756d0ee82459222a332a1e8e38bc9dcc904260d
wassimxd <karimmohsin10@gmail.com>
2023-01-15 19:06:20 +0100

    Keep Hydrated!

--- keep-hydrated.js
+++ keep-hydrated.js
@@ -3,7 +3,7 @@
 
 You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value. */
 // built in func Math.git status
-floor;
+/* floor; */
 function litres(time) {
   return Math.floor(time * 0.5);
 }
