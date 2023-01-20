0123b9b32fcc7916d986cba5894335816f00fb12
wassimxd <karimmohsin10@gmail.com>
2023-01-20 14:26:09 +0100

    Welcome!

---
+++ welcome.js
@@ -0,0 +1,46 @@
+//Welcome!
+/* Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
+
+The Task
+Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
+Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input. */
+//Switch case
+function greet(language) {
+  switch (language) {
+    case "dutch":
+      return "Welkom";
+    case "danish":
+      return "Velkomst";
+    case "italian":
+      return "Benvenuto";
+    case "czech":
+      return "Vitejte";
+    case "estonian":
+      return "Tere tulemast";
+    case "finnish":
+      return "Tervetuloa";
+    case "french":
+      return "Bienvenue";
+    case "german":
+      return "Willkommen";
+    case "irish":
+      return "Failte";
+    case "latvian":
+      return "Gaidits";
+    case "lithuanian":
+      return "Laukiamas";
+    case "polish":
+      return "Witamy";
+    case "spanish":
+      return "Bienvenido";
+    case "swedish":
+      return "Valkommen";
+    case "welsh":
+      return "Croeso";
+    case "flemish":
+      return "Welgekomen";
+    default:
+      return "Welcome";
+  }
+}
+console.log(greet("dutch"));
