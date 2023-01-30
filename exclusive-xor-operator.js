e7059de3fcef90d21efd7dfc7a3010dfc3afb01f
wassimxd <karimmohsin10@gmail.com>
2023-01-30 11:27:05 +0100

    Exclusive or (xor) Logical Operator

---
+++ exclusive-xor-operator.js
@@ -0,0 +1,7 @@
+//Exclusive "or" (xor) Logical Operator
+//Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
+//check if ...
+function xor(a, b) {
+  if (a === true && b === true) return false;
+  return a === true || b === true ? true : false;
+}
