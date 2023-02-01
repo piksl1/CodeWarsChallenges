1fe7c6d45810d2d3f3237c9a2c3aa86fe8760b66
wassimxd <karimmohsin10@gmail.com>
2023-02-01 19:37:14 +0100

    Formatting decimal places #0

---
+++ formatting-decimal.js
@@ -0,0 +1,6 @@
+//Formatting decimal places #0
+/* Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests. */
+function twoDecimalPlaces(n) {
+  return Number(n.toFixed(2));
+}
+console.log(twoDecimalPlaces(5.5589));
