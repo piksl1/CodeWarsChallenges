5c9db1746bbead8ddb0b21a1a235d761078d217e
wassimxd <karimmohsin10@gmail.com>
2023-03-12 17:20:55 +0100

    Reversed Words

---
+++ reversed-words.js
@@ -0,0 +1,10 @@
+//Reversed Words
+
+/*Complete the solution so that it reverses all of the words within the string passed in.
+
+Words are separated by exactly one space and there are no leading or trailing spaces.*/
+
+function reverseWords(str) {
+  return str.split(" ").reverse().join(" ");
+}
+console.log(reverseWords("yoda doesn't speak like this"));
