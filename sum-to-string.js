63793fb207b5c19be5d5cc96c65a8e3b16b7b24d
wassimxd <karimmohsin10@gmail.com>
2023-03-15 16:29:03 +0100

    Sum The Strings

---
+++ sum-to-string.js
@@ -0,0 +1,8 @@
+//Sum The Strings
+
+/*Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+ */
+function sumStr(a, b) {
+  return String(+a + +b);
+}
+console.log(sumStr("4", "5"));
